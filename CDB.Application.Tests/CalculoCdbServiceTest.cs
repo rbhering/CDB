47e8a02aff098c91396bdb3540e431a6b85fa386
rbhering <77896350+rbhering@users.noreply.github.com>
2025-05-05 16:38:54 -0300

    .

--- CDB.Application.Tests/CalculoCdbServiceTest.cs
+++ CDB.Application.Tests/CalculoCdbServiceTest.cs
@@ -55,7 +55,7 @@ public sealed class CalculoCdbServiceTest
     public void CalculoCdbService_CalcularCdb_Deve_Retornar_CdbResponseDto_Com_Valores_Corretos()
     {
         // Arrange  
-        //Estou instanciando os Repositories, porque ele é Fake, caso contraio, não instanciaria os Repositories em um teste 
+        //Estou instanciando os Repositories, porque eles são Fake, caso contraio, não instanciaria os Repositories em um teste 
         var mesesImpostoRepository = new MesesImpostoRepositoryFake();
         var tbCdiRepository = new TbCdiRepositorFake();
 
