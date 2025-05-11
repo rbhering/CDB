0dae9023e2cf332bda4b1f28945d0fcf3a2c01b8
rbhering <77896350+rbhering@users.noreply.github.com>
2025-05-11 18:04:40 -0300

    .

--- CDB.Application/Validators/CdbRequestDtoValidator.cs
+++ CDB.Application/Validators/CdbRequestDtoValidator.cs
@@ -8,10 +8,14 @@ public class CdbRequestDtoValidator : AbstractValidator<CdbRequestDto>
     public CdbRequestDtoValidator()
     {
         RuleFor(x => x.QtdMeses)
+            .NotEmpty()
+            .WithMessage("O campo Quantidade de Meses é obrigatório.")
             .InclusiveBetween(2, 60)
             .WithMessage("O campo Quantidade de Meses deve estar entre 2 e 60 meses.");
 
         RuleFor(x => x.ValorInicial)
+            .NotEmpty()
+            .WithMessage("O campo Valor Inicial é obrigatório.")
             .InclusiveBetween(0.01M, 250000)
             .WithMessage("O campo Valor Inicial  deve estar entre 0,01 e 250.000,00.");
     }
